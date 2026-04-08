b8887c0bc9c6733a66b6a5679be321b746fe4137
Usman Murtaza <usmanmurtaza2004@gmail.com>
2026-04-09 00:56:07 +0500

    issue resolved in project files

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import compression from 'vite-plugin-compression'
-import { createHtmlPlugin } from 'vite-plugin-html'
 
 export default defineConfig({
   plugins: [
@@ -13,43 +12,7 @@ export default defineConfig({
     compression({
       algorithm: 'brotliCompress',
       ext: '.br'
-    }),
-    createHtmlPlugin({
-      minify: true,
-      inject: {
-        data: {
-          title: 'GPA & CGPA Calculator with Scientific Calculator',
-          description: 'Free online GPA, CGPA, and scientific calculator for students'
-        }
-      }
     })
   ],
-  server: {
-    port: 3000,
-    open: true
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    minify: 'terser',
-    chunkSizeWarningLimit: 1000,
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-        pure_funcs: ['console.log']
-      }
-    },
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          emailjs: ['@emailjs/browser']
-        },
-        chunkFileNames: 'assets/js/[name]-[hash].js',
-        entryFileNames: 'assets/js/[name]-[hash].js',
-        assetFileNames: 'assets/[ext]/[name]-[hash].[ext]'
-      }
-    }
-  }
+  // ... rest of config
 })
