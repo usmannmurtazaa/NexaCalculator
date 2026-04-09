14d2e7b53c1d3511b9127be064341499ef2c50cc
Usman Murtaza <usmanmurtaza2004@gmail.com>
2026-04-10 01:36:42 +0500

    add some new features

--- vite.config.js
+++ vite.config.js
@@ -7,12 +7,41 @@ export default defineConfig({
     react(),
     compression({
       algorithm: 'gzip',
-      ext: '.gz'
+      ext: '.gz',
+      threshold: 10240,
+      deleteOriginFile: false
     }),
     compression({
       algorithm: 'brotliCompress',
-      ext: '.br'
+      ext: '.br',
+      threshold: 10240,
+      deleteOriginFile: false
     })
   ],
-  // ... rest of config
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+          'utils': ['jspdf', 'xlsx', '@emailjs/browser']
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000,
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    }
+  },
+  server: {
+    port: 3000,
+    open: true
+  },
+  preview: {
+    port: 4173,
+    open: true
+  }
 })
